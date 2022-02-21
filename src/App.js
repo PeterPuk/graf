279ae8169ac4a6288c283da40a6723e5bbc5b0f5
PeterPuk <pukac4@stud.uniza.sk>
2022-02-21 18:55:58 +0100

    ini

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,LabelList } from 'recharts';
 function App() {
+  const data = [
+  {
+    name: 'Vydareny projekt',
+    price: 45000,
+  },
+  {
+    name: 'Zly projekt',
+    price: 150000,
+
+  },
+  {
+    name: 'Najlepsi projekt',
+    price: 15896,
+
+  }
+];
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Hello</h1>
+      <div className="graf">
+        <h1>Toto je graf</h1>
+      <BarChart
+          width={500}
+          height={300}
+          data={data}
+          
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CartesianGrid strokeDasharray="3 3" backgroundColor={"black"}/>
+          <XAxis dataKey="name" tick={{stroke:"red"}} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="price">
+            <LabelList position="top" fill="white" className="bar"/>
+          </Bar>
+
+        </BarChart>
+      </div>
     </div>
   );
 }
